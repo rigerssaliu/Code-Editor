6d737baa562d9c3944940a1f0f6d80e4057f5afd
rigers.saliu <rigerssaliu@hotmail.com>
2018-08-06 16:39:36 +0200

    Html + Css Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'RSTest';
+  
+  ngOnInit() { 
+
+    $(document).ready(function () {
+      $('.grid').height($(window).height());
+  
+      var frame = $('iframe'),
+        contents = frame.contents(),
+        body = contents.find('body'),
+        styleTag = contents
+          .find('head')
+          .append('<style></style>')
+          .children('style');
+      $('textarea').focus(function () {
+        var $this = $(this);
+        $this.keyup(function () {
+          if ($this.attr('id') === 'html') {
+            body.html($this.val());
+          } else {
+            styleTag.text($this.val());
+          }
+        });
+      });
+    });
+    
+  }
+
+  
 }
